ba1b1dbc0e218b8ca0b01ce9d7b0c65930271df5
Gintautas Plonis <gplonis@gmail.com>
2019-01-25 22:16:30 +0200

    Add google charts.

--- admin/index.php
+++ admin/index.php
@@ -20,6 +20,9 @@ include 'inc/header.php';
                     </h1>
 
                     <?php include "inc/widgets.php"; ?>
+                    <div class="row" style="padding-left: 20px">
+                        <div id="columnchart_material" style="width: 100%;height: 400px"></div>
+                    </div>
                 </div>
             </div>
             <!-- /.row -->
@@ -31,5 +34,39 @@ include 'inc/header.php';
     <!-- /#page-wrapper -->
 
 </div>
+<?php
+$query = mysqli_query($connection, "SELECT * FROM post WHERE status=LOWER('draft')");
+handle_query_error($query);
+$draft_count = mysqli_num_rows($query);
+
+$query = mysqli_query($connection, "SELECT * FROM comment WHERE status=LOWER('unapproved')");
+handle_query_error($query);
+$unapproved_count = mysqli_num_rows($query);
+
+$query = mysqli_query($connection, "SELECT * FROM user WHERE role=LOWER('user')");
+handle_query_error($query);
+$subscriber_count = mysqli_num_rows($query);
+?>
 <!-- /#wrapper -->
+<script type="text/javascript">
+    google.charts.load('current', {'packages': ['bar']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Data', 'Count'],
+            <?php
+            $elements = ['All Posts', 'Draft Posts', 'All Comments', 'Unapproved Comments', 'All Users', 'Subscribers', 'Categories'];
+            $count = [$posts_count, $draft_count, $comments_count, $unapproved_count, $users_count, $subscriber_count, $categories_count];
+
+            for ($i = 0; $i < count($elements); $i++) {
+                echo "['{$elements[$i]}',{$count[$i]}],";
+            }
+            ?>
+        ]);
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+        chart.draw(data, google.charts.Bar.convertOptions());
+    }
+</script>
 <?php include 'inc/footer.php'; ?>
