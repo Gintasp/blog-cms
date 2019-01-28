afb0b3c782e6d91ceac101583ea58db03986e372
Gintautas Plonis <gplonis@gmail.com>
2019-01-28 20:23:41 +0200

    Display post views.

--- post.php
+++ post.php
@@ -15,9 +15,14 @@ include 'inc/navigation.php';
             <?php
             if (isset($_GET['p_id'])) {
                 $current_post_id = $_GET['p_id'];
+
+                $query = mysqli_query($connection, "UPDATE post SET views=views+1 WHERE id=$current_post_id");
+                handle_query_error($query);
+            } else {
+                header("Location: index.php");
             }
 
-            $query = mysqli_query($connection, "SELECT * FROM post WHERE id={$current_post_id} ORDER BY date DESC");
+            $query = mysqli_query($connection, "SELECT * FROM post WHERE id={$current_post_id}");
             handle_query_error($query);
 
             while ($row = mysqli_fetch_assoc($query)) { ?>
@@ -28,6 +33,7 @@ include 'inc/navigation.php';
                     by <a href="#"><?php echo $row['author']; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $row['date']; ?></p>
+                <p style="margin-bottom: 0;font-size: 1.1em">Views <?php echo $row['views']; ?></p>
                 <hr>
                 <img class="img-responsive" src="images/<?php echo $row['image']; ?>" alt="">
                 <hr>
