a7eeee82377ceb91c0ccfc917386574c5a5cb16a
Gintautas Plonis <gplonis@gmail.com>
2019-02-03 09:14:19 +0200

    Update search results.

--- search.php
+++ search.php
@@ -8,15 +8,14 @@ include 'inc/navigation.php';
         <div class="col-md-8">
 
             <h1 class="page-header">
-                Page Heading
-                <small>Secondary Text</small>
+                Search results
             </h1>
 
             <?php
 
             if (isset($_POST['submit'])) {
                 $search = escape($_POST['search']);
-                $query = mysqli_query($connection, "SELECT * FROM post WHERE tags LIKE '%$search%'");
+                $query = mysqli_query($connection, "SELECT * FROM post WHERE tags LIKE '%$search%' AND status='published'");
                 handle_query_error($query);
 
                 if (!mysqli_num_rows($query)) { ?>
@@ -24,32 +23,23 @@ include 'inc/navigation.php';
                 <?php } else {
                     while ($row = mysqli_fetch_assoc($query)) { ?>
                         <h2>
-                            <a href="#"><?php echo $row['title']; ?></a>
+                            <a href="post.php?p_id=<?php echo $row['id']; ?>"><?php echo $row['title']; ?></a>
                         </h2>
                         <p class="lead">
-                            by <a href="index.php"><?php echo $row['author']; ?></a>
+                            by
+                            <a href="author_posts.php?author=<?php echo $row['author']; ?>"><?php echo $row['author']; ?></a>
                         </p>
                         <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $row['date']; ?></p>
                         <hr>
                         <img class="img-responsive" src="images/<?php echo $row['image']; ?>" alt="">
                         <hr>
                         <p><?php echo $row['content']; ?></p>
-                        <a class="btn btn-primary" href="#">Read More <span
+                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $row['id']; ?>">Read More <span
                                     class="glyphicon glyphicon-chevron-right"></span></a>
-
                         <hr>
                     <?php }
                 }
             } ?>
-
-            <ul class="pager">
-                <li class="previous">
-                    <a href="#">&larr; Older</a>
-                </li>
-                <li class="next">
-                    <a href="#">Newer &rarr;</a>
-                </li>
-            </ul>
         </div>
 
         <?php include 'inc/sidebar.php'; ?>
